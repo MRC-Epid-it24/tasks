e487ff5a67feaf908cb25be6542b1ac5976aafaf
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2021-03-10 10:12:25 +0000

    feat(build): build improvements
    - remove cross-env dep and use in-built env webpack functionality
    - add progress bar

--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,54 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const NodemonPlugin = require('nodemon-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const WebpackBar = require('webpackbar');
 
-const { NODE_ENV = 'development' } = process.env;
-const isDev = NODE_ENV === 'development';
+module.exports = (env) => {
+  const { NODE_ENV = 'development' } = env;
+  const isDev = NODE_ENV === 'development';
 
-module.exports = {
-  entry: {
-    index: path.resolve('./src/index.ts'),
-    cli: path.resolve('./src/cli.ts'),
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-  },
-  mode: NODE_ENV,
-  target: 'node',
-  watch: isDev,
-  devtool: isDev ? 'source-map' : undefined,
-  optimization: {
-    minimize: false,
-  },
-  externals: [nodeExternals()],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.json'],
+  return {
+    entry: {
+      index: path.resolve('./src/index.ts'),
+      cli: path.resolve('./src/cli.ts'),
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+    },
+    mode: NODE_ENV,
+    target: 'node',
+    watch: isDev,
+    devtool: isDev ? 'source-map' : undefined,
+    optimization: {
+      minimize: false,
+    },
+    externals: [nodeExternals()],
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.json'],
+      plugins: [
+        new TsconfigPathsPlugin({
+          configFile: './tsconfig.json',
+          logLevel: 'info',
+          logInfoToStdOut: true,
+          extensions: ['.ts'],
+        }),
+      ],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
     plugins: [
-      new TsconfigPathsPlugin({
-        configFile: './tsconfig.json',
-        logLevel: 'info',
-        logInfoToStdOut: true,
-        extensions: ['.ts'],
+      new NodemonPlugin({
+        nodeArgs: ['--inspect=5858'],
       }),
+      new WebpackBar({ name: 'Tasks' }),
     ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new NodemonPlugin({
-      nodeArgs: ['--inspect=5858'],
-    }),
-  ],
+  };
 };
