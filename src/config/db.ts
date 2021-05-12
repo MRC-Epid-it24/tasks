021a1048b3397fb2d2850bbb0f8b6ef965af393f
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2021-05-12 14:53:01 +0100

    fix(db): default encrypt to false
    - tedious package default value has changed to true

--- src/config/db.ts
+++ src/config/db.ts
@@ -39,6 +39,7 @@ const dbConfig: DBConfig = {
     requestTimeout: parseInt(process.env.MSSQL_REQUEST_TIMEOUT || `${60 * 1000}`, 10),
     options: {
       cancelTimeout: parseInt(process.env.MSSQL_CANCEL_TIMEOUT || `${60 * 1000}`, 10),
+      encrypt: false,
     },
   },
 };
